18a627a858ba63de92a3d5d0dda45f499f3edd3f
Jeremy Gervais <jeremy@emitter.com>
2014-07-26 23:03:50 -0400

    Move GSON to Schema
    Gson usage for OB<—>Json serialization is really a common piece for all
    libs. Moved it to schema so if we do something else, i’ll be easier to
    swap it out.
    
    GSON is now removed from all locations.

---
+++ src/org/emitter/utils/JsonUtil.java
@@ -0,0 +1,69 @@
+package org.emitter.utils;
+
+import java.io.Reader;
+import java.io.Writer;
+import java.lang.reflect.Type;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+/**
+ * 
+ * @author Jeremy
+ *
+ */
+public class JsonUtil {
+	static GsonBuilder g = null;
+	static Gson gson = null;
+	private static synchronized void build()
+	{
+		if(gson == null)
+		{
+			if(g == null)
+			{
+				g = new GsonBuilder();
+			}
+			gson = g.create();
+		}
+
+	}
+	
+	private static Gson get()
+	{
+		if(gson == null)
+			build();
+		return gson;
+	}
+	
+	public static <T> void to(T ob, Writer w)
+	{
+		Type t = new TypeToken<T>() {}.getType();
+		get().toJson(ob, t, w);
+	}
+
+	
+	public static <T> String to(T ob)
+	{
+		Type t = new TypeToken<T>() {}.getType();
+		return get().toJson(ob, t);
+	}
+	
+	public static String to(Object ob, Type t)
+	{
+		return get().toJson(ob, t);
+	}
+	
+	public static <T> T from(String j)
+	{
+		return get().fromJson(j,new TypeToken<T>() {}.getType());
+	}
+
+	public static <T> T from(Reader r)
+	{
+		Type classOfT = null;
+		return get().fromJson(r, classOfT);
+	}
+	
+	
+}
